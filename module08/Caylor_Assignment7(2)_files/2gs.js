db97014f7a19fff888048e44dfcd229061b30fee
Clay Caylor <wigshaker@gmail.com>
2017-11-08 21:06:57 -0600

    module 08?

---
+++ module08/Caylor_Assignment7(2)_files/2gs.js
@@ -0,0 +1,9 @@
+// Qualaroo for phonegap.com
+// (C) 2016 Qualaroo. All rights reserved.
+// qualaroo.com
+
+//$ site: 9244, generated: 2016-05-27 23:34:02 UTC
+//$ type: base, rev. 546d0e52 (deploy)
+//$ client: 2.0.14
+
+//Your Qualaroo account is suspended or disabled.  Please see your dashboard at https://app.qualaroo.com (or email support@qualaroo.com).
